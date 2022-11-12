6bb330a98110ce585e549d6c9b917a4e4f353e2a
PhotikLed <ledlightsuper@yandex.ru>
2022-11-12 18:50:24 +0500

    Решение задачи 2

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from PyQt5.QtCore import QPoint
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication, QLabel
 import sys
+import random
 from UI import Ui_Form
 
 
@@ -7,6 +10,22 @@ class Example(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.draw_circle)
+
+    def draw_circle(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setBrush(QColor('yellow'))
+            r = random.randint(20, 100)
+            qp.drawEllipse(QPoint(200, 200), r, r)
+            qp.end()
+            self.do_paint = False
 
 
 def except_hook(cls, exception, traceback):
